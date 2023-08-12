2514226a67f443df1b1976e9d722fb492d5f802b
manhburn <quymanhtran453@gmail.com>
2023-08-13 01:44:20 +0700

    50% video

--- src/components/Video/ContentVideo/ContentVideo.js
+++ src/components/Video/ContentVideo/ContentVideo.js
@@ -1,17 +1,40 @@
 import classNames from 'classnames/bind';
 import styles from './ContentVideo.mudule.scss';
+import Tippy from '@tippyjs/react/headless';
+import { Wrapper as OptionWrapper } from '../../Popper';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEllipsisH, faEllipsisVertical, faPause, faPlay } from '@fortawesome/free-solid-svg-icons';
-import { Volume, XmarkVolume } from '../../Icon';
+import { faEllipsisH, faPause, faPlay } from '@fortawesome/free-solid-svg-icons';
+import { BrokenHeartIcon, ReportIcon, Volume, XmarkVolume } from '../../Icon';
 import { useEffect, useRef, useState } from 'react';
+import Button from '../../Button';
 
 const cx = classNames.bind(styles);
 function ContentVideo() {
     const playController = useRef();
     const [isEnter, setIsEnter] = useState(false);
+    const [isEnterTippy, setIsEnterTippy] = useState(false);
     const [isPlaying, setIsPlaying] = useState(true);
     const [isMute, setIsMute] = useState(true);
     // handle functions
+
+    const renderPreview = (attrs) => {
+        return (
+            <div tabIndex="-1" {...attrs}>
+                <OptionWrapper>
+                    <div className={cx('video-view')}>
+                        <Button className={cx('not-interested')} leftIcon={<BrokenHeartIcon></BrokenHeartIcon>}>
+                            Not interested
+                        </Button>
+
+                        <Button className={cx('report')} leftIcon={<ReportIcon></ReportIcon>}>
+                            Report
+                        </Button>
+                    </div>
+                </OptionWrapper>
+            </div>
+        );
+    };
+
     const handlePlay = () => {
         if (playController.current.paused) {
             playController.current.play();
@@ -30,8 +53,12 @@ function ContentVideo() {
         <div className={cx('wrapper')}>
             <div
                 className={cx('video-wrapper')}
-                onMouseEnter={() => setIsEnter(true)}
-                onMouseLeave={() => setIsEnter(false)}
+                onMouseEnter={() => {
+                    setIsEnter(true);
+                }}
+                onMouseLeave={() => {
+                    setIsEnter(false);
+                }}
             >
                 <video
                     ref={playController}
@@ -42,29 +69,47 @@ function ContentVideo() {
                     muted={isMute}
                     loop
                 ></video>
-                {isEnter && (
+                {(isEnter || isEnterTippy) && (
                     <div className="play-controller" onClick={handlePlay}>
                         {!isPlaying && <FontAwesomeIcon icon={faPlay} className="play-icon" />}
                         {isPlaying && <FontAwesomeIcon icon={faPause} className="pause-icon" />}
                     </div>
                 )}
-                {isEnter && (
-                    <div className={cx('option-controller')}>
-                        <FontAwesomeIcon icon={faEllipsisH} />
+                {(isEnter || isEnterTippy) && (
+                    <div>
+                        <Tippy
+                            interactive
+                            offset={[20, 30]}
+                            delay={[0, 100]}
+                            placement="right-start"
+                            render={renderPreview}
+                            onMount={() => {
+                                setIsEnterTippy(true);
+                            }}
+                            onHidden={() => {
+                                setIsEnterTippy(false);
+                            }}
+                        >
+                            <div className={cx('option-controller')}>
+                                <FontAwesomeIcon icon={faEllipsisH} />
+                            </div>
+                        </Tippy>
                     </div>
                 )}
-                {isEnter && (
+                {(isEnter || isEnterTippy) && (
                     <div className={cx('volume-controller')} onClick={handleMute}>
                         {!isMute && <Volume className={cx('volume-on')}></Volume>}
                         {isMute && <XmarkVolume className={cx('volume-off')}></XmarkVolume>}
                     </div>
                 )}
-                {/* <div className={cx('seek-controller')}>
-                    <progress className="seek-bar" type="progress" value={'50'} max={'100'}></progress>
-                    <div className={cx('time')}>
-                        <span className="real-time">00:45</span>/<span className="total-time">00:60</span>
+                {(isEnter || isEnterTippy) && (
+                    <div className={cx('seek-controller')}>
+                        <input className="seek-bar" type="range" value="50" step="1" min="0" max="100"></input>
+                        <div className={cx('time')}>
+                            <span className="real-time">00:45</span>/<span className="total-time">00:60</span>
+                        </div>
                     </div>
-                </div> */}
+                )}
             </div>
         </div>
     );

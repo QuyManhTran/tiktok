3c526fe2370ce45dce63e8b772c9196fe3383599
manhburn <quymanhtran453@gmail.com>
2023-08-15 19:41:10 +0700

    css for action sharing

--- src/components/Video/ActionItems/ActionItems.js
+++ src/components/Video/ActionItems/ActionItems.js
@@ -1,15 +1,41 @@
 import classNames from 'classnames/bind';
 import styles from './ActionItems.module.scss';
 import Tippy from '@tippyjs/react/headless';
-import { Wrapper as ShareLink } from '../../Popper';
 import ActionItem from '../../ActionItem';
+import { Wrapper as ShareLink } from '../../Popper';
 import { HeartIcon, CommentIcon, ShareIcon, FavoriteIcon } from '../../Icon/Icons';
+import Button from '../../Button';
+import { EmbedIcon, SendIcon, WhatsAppIcon, CopyLinkIcon, FacebookIcon } from '../../Icon/Icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+
 const cx = classNames.bind(styles);
 function ActionItems() {
     const sharingLink = (attrs) => {
         return (
             <div tabIndex="-1" {...attrs}>
-                <ShareLink>content</ShareLink>
+                <ShareLink>
+                    <div className={cx('link-items')}>
+                        <Button leftIcon={<EmbedIcon></EmbedIcon>} className={cx('link-item')}>
+                            <span> Embed</span>
+                        </Button>
+                        <Button leftIcon={<SendIcon></SendIcon>} className={cx('link-item')}>
+                            <span>Send to friends</span>
+                        </Button>
+                        <Button leftIcon={<FacebookIcon></FacebookIcon>} className={cx('link-item')}>
+                            <span>Share to Facebook</span>
+                        </Button>
+                        <Button leftIcon={<WhatsAppIcon></WhatsAppIcon>} className={cx('link-item')}>
+                            <span>Share to WhatsApp</span>
+                        </Button>
+                        <Button leftIcon={<CopyLinkIcon></CopyLinkIcon>} className={cx('link-item')}>
+                            <span>Copy Link</span>
+                        </Button>
+                        <div className={cx('link-item')}>
+                            <FontAwesomeIcon icon={faAngleDown} className={cx('down-icon')} />
+                        </div>
+                    </div>
+                </ShareLink>
             </div>
         );
     };
@@ -22,7 +48,7 @@ function ActionItems() {
             <ActionItem icon={<FavoriteIcon></FavoriteIcon>} active={cx('yellow-active')}>
                 92.8K
             </ActionItem>
-            <Tippy interactive placement="bottom-end" render={sharingLink}>
+            <Tippy interactive placement="top-start" offset={[-16, 0]} render={sharingLink}>
                 <div>
                     <ActionItem icon={<ShareIcon></ShareIcon>}>39.1K</ActionItem>
                 </div>
